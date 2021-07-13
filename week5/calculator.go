1e59667298033b54422dab966806a01077096ce1
fcwrwen <fcwrwen@gmail.com>
2021-07-13 17:43:43 +0000

    new

--- week5/calculator.go
+++ week5/calculator.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var (
 		n1, n2, s1 string
-	) //n-number,s-symbol
+	)
 	fmt.Println("Please input your request for RPN calculator: ")
 	fmt.Scanln(&n1, &n2, &s1)
 	n := []string{n1, n2, s1}
